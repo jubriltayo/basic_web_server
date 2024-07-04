84d9a2907ec2498b4acd1fb010de28798fcaf214
Tayo <jubriltayo@gmail.com>
2024-07-04 16:20:22 +0100

    ip and location issue fixed

--- api/views.py
+++ api/views.py
@@ -1,22 +1,24 @@
 import requests
-from django.conf import settings
 from django.http import JsonResponse
 import os
 
 def hello_view(request):
     visitor_name = request.GET.get('visitor_name', 'Visitor')
 
-    # GET IP 
+    # GET CLIENT'S ORIGINAL IP ADDRESS
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR') # contains IP addresses
+    if x_forwarded_for:
+        client_ip = x_forwarded_for.split(',')[0] # the first being the original client's IP address not the host
+    else:
+        client_ip = request.META.get('REMOTE_ADDR') # 127.0.0.1 or localhost
+
+    # MAKE API CALLS WITH THE CLIENT'S IP RETRIEVED
     ip_api_key = os.getenv('IPINFO_API_KEY')
     # send request to the api to get response
-    ipinfo_url = f'https://ipinfo.io/json?token={ip_api_key}'
+    ipinfo_url = f'https://ipinfo.io/{client_ip}/json?token={ip_api_key}'
     ip_response = requests.get(ipinfo_url).json()
-    if settings.DEBUG: # for localhost
-        client_ip = request.META.get('REMOTE_ADDR')
-    else:
-        client_ip = ip_response.get('ip')
     # get location
-    location = ip_response.get('city')
+    location = ip_response.get('city', 'Unknown')
 
     # GET TEMPERATURE
     openweather_api_key = os.getenv('OPENWEATHERAPI_API_KEY')
